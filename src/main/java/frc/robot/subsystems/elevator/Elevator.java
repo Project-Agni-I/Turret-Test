1b09819bcfab339eb7703f49c31fbe141aa2d80d
khushcorp <k6605922@gmail.com>
2025-06-23 22:52:19 -0400

    Add turret subsystem and modify elevator subsystem

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.elevator.ElevatorIO;
-import frc.robot.subsystems.elevator.ElevatorIOInputsAutoLogged;
 
 public class Elevator extends SubsystemBase {
 	private final ElevatorIO io;
